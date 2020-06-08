c27996ad5722708d80c91690cdcd5c87baf7c274
万灵杰 <tzjack@live.cn>
2020-06-08 17:35:22 +0800

    Channel,Target

---
+++ BaseSolution/Terminal.Collector.Core/Channel.cs
@@ -0,0 +1,41 @@
+﻿using S7.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal.Collector.Core
+{
+    /// <summary>
+    /// 数据采集通道
+    /// </summary>
+    public class Channel: Plc,IDisposable
+    {
+        public string Name { private set; get; }
+
+        public Channel(string name,CpuType cpu, string ip, short rack, short slot)
+            :base(cpu, ip, rack, slot)
+        {
+            Name = name;
+        }
+
+        public Channel(string name, CpuType cpu, string ip, int port, short rack, short slot)
+            : base(cpu, ip, port, rack, slot)
+        {
+            Name = name;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                if (this.IsConnected)
+                {
+                    this.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ BaseSolution/Terminal.Collector.Core/Target.cs
@@ -0,0 +1,15 @@
+﻿using S7.Net.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal.Collector.Core
+{
+    /// <summary>
+    /// 数据采集点
+    /// </summary>
+    public class Target: DataItem
+    {
+        public string Name { set; get; }
+    }
+}
