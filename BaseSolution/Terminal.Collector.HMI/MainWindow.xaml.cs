88b5399cf47d871046c5a8740ba672cec1573c1e
万灵杰 <tzjack@live.cn>
2020-06-17 11:19:08 +0800

    NotifyIcon

--- BaseSolution/Terminal.Collector.HMI/MainWindow.xaml.cs
+++ BaseSolution/Terminal.Collector.HMI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -21,13 +23,61 @@ namespace Terminal.Collector.HMI
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        //实例化notifyIOC控件最小化托盘
+        private NotifyIcon _notifyIcon = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitialTray();
         }
 
-        private void Menu_Button_Click(object sender, RoutedEventArgs e)
+        #region 最小化系统托盘
+        private void InitialTray()
         {
+            //隐藏主窗体
+            this.Visibility = Visibility.Hidden;
+            //设置托盘的各个属性
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.BalloonTipText = "服务运行中...";//托盘气泡显示内容
+            _notifyIcon.Text = "ServerApp";
+            _notifyIcon.Visible = true;//托盘按钮是否可见
+            _notifyIcon.Icon = new Icon(@"favicon.ico");//托盘中显示的图标
+            _notifyIcon.ShowBalloonTip(2000);//托盘气泡显示时间
+            _notifyIcon.MouseDoubleClick += _notifyIcon_MouseDoubleClick; ;
+            //窗体状态改变时触发
+            this.StateChanged += MainWindow_StateChanged;
         }
+        
+        #endregion
+
+        #region 窗口状态改变
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.Visibility = Visibility.Hidden;
+            }
+        }
+        #endregion
+
+        #region 托盘图标鼠标单击事件
+        private void _notifyIcon_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (this.Visibility == Visibility.Visible)
+                {
+                    this.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    this.Visibility = Visibility.Visible;
+                    this.WindowState = WindowState.Normal;
+                    this.Activate();
+                }
+            }
+        }
+        #endregion
     }
 }
