2e2d99e1f882ea780eef73950e73d2f460744cb8
万灵杰 <tzjack@live.cn>
2020-06-28 10:01:21 +0800

    grpc

---
+++ BaseSolution/Sample.GrpClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using System;
+
+namespace Sample.GrpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greet.GreeterClient(channel);
+            //var response = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+
+            //Console.WriteLine("Greeting: " + response.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
